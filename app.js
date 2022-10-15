fa11e1222879e56b1e818b40bc35650328a74e4b
pdias-felipe <pdias.felipe@gmail.com>
2022-10-15 12:23:07 -0300

    servidor rodando na porta 3000

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require ('express')
+const app = express();
+
+
+//  teste do servidor
+//  app.get('/', (req, res) => {
+//      res.send ('hello modafocaa!!')
+//  })
+
+
+app.listen(3000, () => console.log('server rodando na porta 3000'));
+
+
+
