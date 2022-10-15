5e76481ea980d7095be65917ac5c615dd05ca769
pdias-felipe <pdias.felipe@gmail.com>
2022-10-15 18:50:56 -0300

    testando as rotas no insomnia - ok

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,31 @@ const express = require('express');
 
 const routes = express.Router();
 
+
+//rotas psicologo (terminar)
 routes.get('/psicologos', (req, res) => {res.send ('hellooo modafocaa!!')});
 routes.get('/psicologo/:id', (req, res) => {res.send ('hellooo modafocaa!!')});
-routes.post('/psicologos', (req, res) => {res.send ('hellooo modafocaa!!')});
-routes.put('/psicologo/:id', (req, res) => {res.send ('hellooo modafocaa!!')});
-routes.delete('/psicologo/:id', (req, res) => {res.send ('hellooo modafocaa!!')});
+
+routes.post('/psicologos', (req, res) => {
+    console.log(req.body) 
+    res.json (req.body)
+});
+
+routes.put('/psicologo/:id', (req, res) => {
+    console.log(req.body) 
+    res.json(req.body)
+});
+
+routes.delete('/psicologo/:id', (req, res) => {res.send ('psicologo deletado')});
+
+
+//rotas pacientes
+
+
+
+//rotas atendimentos
+
+
 
 
 
