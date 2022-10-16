42b32491237cd83e2c0d1c3f93eb85d0d3a813aa
pdias-felipe <pdias.felipe@gmail.com>
2022-10-16 17:25:25 -0300

    models - Psicologos.js ok

---
+++ src/models/psicologos.js
@@ -0,0 +1,41 @@
+const db = require('../database');
+const { DataTypes } = require('sequelize')
+
+const Psicologos = db.define('Psicologos', 
+{
+    id: {
+        type: DataTypes.INTEGER, 
+        primaryKey: true,
+        autoIncrement: true,
+    },
+
+    nome: {
+        type: DataTypes.STRING // VARCHAR no js fica STRING
+    },
+
+    email: {
+        type: DataTypes.STRING
+    },
+
+    senha: {
+        type: DataTypes.STRING
+    },
+
+    apresentação: {
+        type: DataTypes.STRING
+    },
+
+    createdAt: {
+        type: DataTypes.DATE,
+    },
+
+    updatedAt: {
+        type: DataTypes.DATE,
+    },
+}, 
+{
+   tableName: 'psicologos',
+});
+
+
+module.exports = Psicologos;
