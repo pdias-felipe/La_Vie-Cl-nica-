1ed16b0c3e9095ec98aa0d5174d977644551cbb1
pdias-felipe <pdias.felipe@gmail.com>
2022-10-18 20:55:56 -0300

    atualização para fazer o merge

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,15 +1,9 @@
 const express = require('express');
-const authController = require('../controllers/authController');
 const psicologosController = require('../controllers/psicologosController')
-<<<<<<< HEAD
-const requestLog = require("../middleware/requestLog");
-const authLoginValidation = require("../validations/auth/login");
-=======
 const pacientesController = require('../controllers/pacientesController');
 const atendimentosController = require('../controllers/atendimentosController');
 
 
->>>>>>> d121a2f88bd27e6406c002f7d9ac2b540d4a9198
 const routes = express.Router();
 
 
@@ -17,10 +11,9 @@ const routes = express.Router();
 routes.get('/psicologos', psicologosController.listarPsicologos)
 routes.get('/psicologos/:id', psicologosController.listarPsicologoId);
 routes.post('/psicologos', psicologosController.cadastrarPsicologo);
-routes.post('/psicologos/login', requestLog, psicologosController.loginPsicologo);
 routes.put('/psicologos/:id', psicologosController.atualizarPsicologo);
 routes.delete('/psicologos/:id', psicologosController.deletarPsicologo);
-routes.post('/login/', requestLog, authLoginValidation, authController.login);
+
 
 //rotas pacientes
 routes.get('/pacientes', pacientesController.listarPacientes)
