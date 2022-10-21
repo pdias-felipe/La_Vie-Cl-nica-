b5dc3e2e78214c96165a5f0b53ffa2f2373e136f
Yan Martins <yanmartins@github.com>
2022-10-20 23:58:38 -0300

    HandleError

--- src/middleware/handleError.js
+++ src/middleware/handleError.js
@@ -2,6 +2,10 @@ const { ValidationError } = require("express-validation");
 
 module.exports = (error, req, res, next) => {
     if(error instanceof ValidationError){
-        return res.status(401).json("E-mail ou senha inválido, verifique e tente novamente")
+        return res.status(error.statusCode).json("E-mail ou senha inválido, verifique e tente novamente", error);
+    }
+    if(error instanceof ValidationError){
+        return res.status(error.statusCode).json("Id não encontrado")
     }
 };
+
